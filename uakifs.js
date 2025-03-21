aa4d81f9483627a41a229f1f57793d1110fb9fc4
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2025-03-21 23:44:39 +0900

    Update uakifs.js

--- uakifs.js
+++ uakifs.js
@@ -1,13 +1,1 @@
-$(document).ready(function () {
-    const fleftDiv = document.querySelector('.lefts');
-        if (fleftDiv) {
-            if (!fleftDiv.innerHTML.includes('Sui Design')) {
-                const pElement = document.createElement('p');
-                pElement.textContent = 'Copyright Sui Design';
-                fleftDiv.appendChild(pElement);
-            }
-        } else {
-            window.location.href = 'https://suiblog.site';
-        }
-});
-
+$(document).ready(function () { const fleftDiv = document.querySelector('.lefts'); if (fleftDiv) { if (!fleftDiv.innerHTML.includes('Sui Design')) { const p = document.createElement('p'); p.textContent = 'Copyright Sui Design'; fleftDiv.appendChild(p); } } else { window.location.href = 'https://suiblog.site'; } if (window.location.hostname.startsWith("linair")) window.location.href = "http://warning.or.kr/"; });
